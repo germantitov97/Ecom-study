b3a820f58b7802fa54c7b5964130a8d99228942a
germantitov97 <germangood2@gmail.com>
2026-01-27 23:07:24 +0200

    Add files via upload

---
+++ hw germantitov 2501 question1.py
@@ -0,0 +1,20 @@
+# start
+number = int(input("put a number from 0 to 100: "))
+if number > 100 or number < 0:
+    print("invalid")
+elif number <= 100 and number >= 80:
+    print("very good")
+elif number < 80 and number >= 60:
+    print("not bad")
+elif number < 60 and number >= 40:
+    print("try harder")
+else:
+    print("need more exercise")
+# stop
+
+
+
+
+
+
+
---
+++ hw germantitov 2501 question2.py
@@ -0,0 +1,13 @@
+row = int(input("how many * in a rows? "))
+column = int(input("how many columns? "))
+i = 0
+while i < column:
+    print("*" * row)
+    i += 1
+
+
+
+
+
+
+
