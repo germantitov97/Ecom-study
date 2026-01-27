7b69929fc7a0a8311bc33d532526920f22e1625f
germantitov97 <germangood2@gmail.com>
2026-01-27 20:39:14 +0200

    hw germantitov 2101 question5.py

--- hw germantitov 2101 question5.py
+++ hw germantitov 2101 question5.py
@@ -9,34 +9,3 @@ while index != students_count:
     index = index + 1
     total_grade = total_grade + grade
 print("the average grade is:", total_grade / students_count)
-
-
-
-
-
-
-
-i = 0
-highest_rate = 0
-lowest_rate = 5
-while i < 10:
-    rate = int(input("rate the movie"))
-    if rate > 5 or rate < 1:
-        continue
-    else:
-        i = i + 1
-    if rate >= highest_rate:
-        highest_rate = rate
-    if rate <= lowest_rate:
-        lowest_rate = rate
-print("The highest rate is", highest_rate)
-print("The lowest rate is", lowest_rate)
-# stop
-
-
-
-
-
-
-
-
